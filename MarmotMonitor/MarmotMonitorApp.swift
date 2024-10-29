d6a89144e28f6d6126e5f869816c29d50f4f51e8
pierrick <pierrickviret@gmail.com>
2024-10-29 13:23:12 +0100

    Initial Commit

---
+++ MarmotMonitor/MarmotMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarmotMonitorApp.swift
+//  MarmotMonitor
+//
+//  Created by pierrick viret on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarmotMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarmotMonitorTests/MarmotMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarmotMonitorTests.swift
+//  MarmotMonitorTests
+//
+//  Created by pierrick viret on 29/10/2024.
+//
+
+import Testing
+@testable import MarmotMonitor
+
+struct MarmotMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
