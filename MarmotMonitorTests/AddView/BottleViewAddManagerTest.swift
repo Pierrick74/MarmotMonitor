a3a018248a470817bc4204d3398fa50ec3742fda
pierrick <pierrickviret@gmail.com>
2024-11-30 14:18:05 +0100

    feat: add test for bottleManager

---
+++ MarmotMonitorTests/AddView/BottleViewAddManagerTest.swift
@@ -0,0 +1,16 @@
+//
+//  BottleViewAddManagerTest.swift
+//  MarmotMonitorTests
+//
+//  Created by pierrick viret on 29/11/2024.
+//
+
+import Testing
+
+struct BottleViewAddManagerTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MarmotMonitorTests/Mock/MockAppStorage.swift
@@ -0,0 +1,8 @@
+//
+//  MockAppStorage.swift
+//  MarmotMonitorTests
+//
+//  Created by pierrick viret on 29/11/2024.
+//
+
+import Foundation
