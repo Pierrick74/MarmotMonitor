f112398395532166ef3fe55bbece304b5877906c
pierrick <pierrickviret@gmail.com>
2024-12-23 15:05:08 +0100

    feat: add icon selection

---
+++ MarmotMonitor/SetupView/IconView/AppIconTheme.swift
@@ -0,0 +1,8 @@
+//
+//  AppIconTheme.swift
+//  MarmotMonitor
+//
+//  Created by pierrick viret on 23/12/2024.
+//
+
+import Foundation
---
+++ MarmotMonitor/SetupView/IconView/IconView.swift
@@ -0,0 +1,18 @@
+//
+//  IconView.swift
+//  MarmotMonitor
+//
+//  Created by pierrick viret on 23/12/2024.
+//
+
+import SwiftUI
+
+struct IconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconView()
+}
