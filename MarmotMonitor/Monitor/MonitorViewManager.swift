d61b3891a7c4f23740d31b3746801cb63b5315ac
pierrick <pierrickviret@gmail.com>
2025-01-14 09:19:15 +0100

    refact : refact code of monitor view manager

--- MarmotMonitor/Monitor/MonitorViewManager.swift
+++ MarmotMonitor/Monitor/MonitorViewManager.swift
@@ -6,18 +6,22 @@
 //
 
 import SwiftUI
-
+/// Manager for the MonitorView
 @MainActor
 class MonitorViewManager: ObservableObject {
+    // MARK: - Dependencies
     private var dataManager: SwiftDataManagerProtocol = SwiftDataManager.shared
 
+    // MARK: - Published Properties
     @Published var formattedActivityData: [Date: [ActivityRange]] = [:]
     @Published private var filter: [ActivityCategory] = [.diaper, .food, .sleep]
 
+    // MARK: - Computed Properties
     var isSleepSelected: Bool { filter.contains(.sleep) }
     var isDiaperSelected: Bool {filter.contains(.diaper) }
     var isFoodSelected: Bool { filter.contains(.food) }
 
+    // MARK: - Init
     init(dataManager: SwiftDataManagerProtocol? = nil) {
         if let dataManager = dataManager {
             self.dataManager = dataManager
@@ -26,6 +30,9 @@ class MonitorViewManager: ObservableObject {
     }
 
     // MARK: - Functions
+    /// Loads the activities in the date range and formats them for display.
+    /// - Note: This function is called when the view is initialized.
+    /// - Note: This function is called when the filter is toggled.
     func loadActivitiesInDateRange() {
         formattedActivityData = [:]
         let babyActivities = dataManager.fetchFilteredActivities(with: filter)
@@ -45,6 +52,8 @@ class MonitorViewManager: ObservableObject {
         }
     }
 
+    /// Toggles the filter for the given category.
+    /// - Parameter category: The category to toggle the filter for.
     func toggleFilter(_ category: ActivityCategory) {
         if filter.contains(category) {
             filter.removeAll { $0 == category }
@@ -132,36 +141,36 @@ class MonitorViewManager: ObservableObject {
         }
     }
 
+    /// Adds the range to the formatted activity data.
     private func addToFormattedActivityData(_ range: ActivityRange, for date: Date) {
-        if formattedActivityData[date] != nil {
-            formattedActivityData[date]?.append(range)
-        } else {
-            formattedActivityData[date] = [range]
-        }
+        formattedActivityData[date, default: []].append(range)
     }
 
+    /// Transforms a date into an index in the range [0, 47] for display purposes.
     private func transformInRangeIndex(_ date: Date) -> Int {
-        let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: date)
-        let minute = calendar.component(.minute, from: date)
-
-        let totalMinutes = (hour * 60) + minute
+        let components = Calendar.current.dateComponents([.hour, .minute], from: date)
+        let totalMinutes = (components.hour ?? 0) * 60 + (components.minute ?? 0)
         return totalMinutes / 30
     }
 
+    /// Formats the duration of the activity.
+    /// - Parameter seconds: The duration of the activity in seconds.
+    /// - Returns: The formatted duration in Double
     private func formatDuration(_ seconds: TimeInterval) -> Double {
         let hours = Int(seconds) / 3600
         let minutes = Int(seconds) % 3600 / 60
 
-        if minutes == 0 {
+        switch minutes {
+        case 0:
             return Double(hours)
-        } else if minutes < 30 {
+        case 1..<30:
             return Double(hours) + 0.5
-        } else {
+        default:
             return Double(hours + 1)
         }
     }
 
+    /// Checks if the day has changed during the activity.
     private func checkIfDayNotChanged(with date: Date, duration: TimeInterval) -> Bool {
         let calendar = Calendar.current
         let startOfDay = calendar.startOfDay(for: date)
